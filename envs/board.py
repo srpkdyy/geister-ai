7d44df676d0304539142c5f7ec3a87bfd219e8ca
srpkdyy <srpkdyy@gmail.com>
2022-01-19 22:40:50 +0900

    add legal move func

--- envs/board.py
+++ envs/board.py
@@ -12,6 +12,13 @@ class Board:
         PIECE_NUM = 8
         COLOR_NUM = 2
 
+        # UP, LEFT, RIGHT, DOWN
+        self.direction = (
+            (-Board.WIDTH,),
+            (-1,),
+            (+1,),
+            (+Board.WIDTH,))
+
         # Blue: False, Red: True; [A,...,h]
         self.red = np.zeros(PLAYER_NUM * PIECE_NUM, dtype=bool)
         self.red[red] = True
@@ -21,13 +28,13 @@ class Board:
         self.pos = self.toindex(np.array(pos, dtype=np.int16))
         self.pos = self.pos.reshape(PLAYER_NUM, PIECE_NUM)
 
-        # was taken; Bool; [A,...,h]
+        # was taken; Bool; [[A,...,H],[a,...,h]
         self.taken = self.pos == Board.TOMB
 
         # was taken; [[B, R], [b, r]]
         b, r = self.taken[~self.red], self.taken[self.red]
         t = PIECE_NUM // COLOR_NUM
-        self.n_taken = np.count_nonzero([b[:t], r[:t], b[t:], r[t:]], axis=1)
+        self.n_taken = np.count_nonzero((b[:t],r[:t],b[t:],r[t:]), axis=1)
         self.n_taken = self.n_taken.reshape(PLAYER_NUM, COLOR_NUM)
 
     def toindex(self, p):
@@ -36,15 +43,14 @@ class Board:
     def swap(self):
         self.red = self.red[::-1]
 
-        idx = self.pos < Board.MAX_IDX
-        self.pos[idx] = Board.MAX_IDX - self.pos[idx] - 1
+        self.pos = np.where(self.pos < Board.MAX_IDX, Board.MAX_IDX-self.pos-1, self.pos)
         self.pos = self.pos[::-1]
 
         self.taken = self.taken[::-1]
 
         self.n_taken = self.n_taken[::-1]
 
-    def game_state(self):
+    def state(self):
         channels = 3
         pos = self.pos
         red = self.red
@@ -61,7 +67,23 @@ class Board:
 
 
     def legal_move_index(self):
-        pass
+        ally = self.pos[0]
+        edge = Board.WIDTH - 1
+
+        moved = ~np.isin(ally + self.direction, ally)
+        on_board = ((
+            ally // Board.WIDTH > 0,
+            ally % Board.WIDTH > 0,
+            ally % Board.WIDTH < edge,
+            ally // Board.WIDTH < edge))
+
+        can_move = moved & on_board & ~self.taken[0]
+
+        # if can goal
+        can_move[0] |= (ally == 0) | (ally == edge) & ~self.red[0]
+        
+        print(can_move)
+
 
     def move(self, move_i):
         pass
@@ -74,16 +96,20 @@ class Board:
         print('Red:\n', self.red)
         print('Position:\n', self.pos)
         print('Taken:\n', self.taken)
-        print('Board:\n', self.game_state())
+        print('Board:\n', *self.state())
 
 
 if __name__ == '__main__':
-    red = [0, 1, 6, 7, 8, 11, 13, 15]
-    pos = [99, 24, 99, 44, 15, 25, 35, 45, 41, 31, 21, 11, 40, 30, 20, 99]
+    red = [0, 1, 2, 3, 8, 9, 10, 11]
+    pos = [00, 24, 34, 44, 15, 25, 35, 50,
+            55, 31, 21, 11, 40, 30, 20, 5]
 
     b = Board(red, pos)
     b.disp()
 
+    b.legal_move_index()
+
     b.swap()
     b.disp()
+    b.legal_move_index()
 
