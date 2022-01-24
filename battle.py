b306d9ed960f425091621833d60622367502b4c4
srpkdyy <srpkdyy@gmail.com>
2022-01-24 13:38:24 +0900

    xx

--- battle.py
+++ battle.py
@@ -1,44 +1,68 @@
 import numpy as np
 from tqdm import tqdm
 from envs.geister import Geister
-from envs.cgeister import CGeister
+from envs.cgeister import cGeister
 from agents.random.agent import Random
 
-def battle(env, agent0, agent1):
-    state = env.reset(agent0.init_red(), agent1.init_red())
 
+def play(env, agent0, agent1, turn):
+    state = env.reset(agent0.init_red(), agent1.init_red())
     agents = [agent0, agent1]
 
     p = 0
-    while not env.done:
+    for i in range(turn):
         legal_act = env.get_legal_actions()
-
-        act = agents[p].get_policy(state, legal_act)
-
+        act = agents[p].get_action(state, legal_act)
         state = env.step(act)
+        p ^= 1
+
+        if env.done:
+            return 1 if env.winner == 0 else -1
+    return 0
+
+
+def play_history(env, agent0, agent1, turn):
+    state = env.reset(agent0.init_red(), agent1.init_red())
+    agents = [agent0, agent1]
+
+    history = []
+
+    p = 0
+    r = 0
+    for i in range(turn):
+        legal_act = env.get_legal_actions()
+        action = agents[p].get_action(state, legal_act)
+
+        history.append([state, action, None, None])
+
+        state = env.step(action)
+        history[i][-1] = state
 
         p ^= 1
 
-    return env.winner, env.turn
+        if env.done:
+            r = 1 if env.winner == 0 else -1
+            break
+
+    # last step is first player
+    r = r if turn % 2 else -r
+
+    history[-1][-2] = r
+    history[-2][-2] = -r
+    return history
+
 
 
 
 if __name__ == '__main__':
-    n = 100000
-    #env = Geister()
-    env = CGeister()
+    n = 1
+    env = cGeister()
     agent0 = Random()
     agent1 = Random()
 
     rate = 0
     turn = []
     for _ in tqdm(range(n)):
-        r, t =  battle(env, agent0, agent1)
-        rate += r
-        turn.append(t)
-    print(env.render())
-
-    print(1 - rate/n)
-    turn = np.array(turn)
-    print(turn.min(), turn.mean(), turn.max())
+        h = play_history(env, agent0, agent1, 200)
+    print(h)
 
